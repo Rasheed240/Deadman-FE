c0e36b385f74962f7187b0ba63220b91e7d8303a
Rasheed (Sauce account) <brasheed240@gmail.com>
2025-03-08 10:00:00 +0100

    create API barrel export

---
+++ src/lib/api/index.ts
@@ -0,0 +1,10 @@
+/**
+ * Central API exports
+ */
+export * from './client';
+export * from './auth';
+export * from './messages';
+export * from './recipients';
+export * from './checkins';
+export * from './payments';
+export * from './analytics';
